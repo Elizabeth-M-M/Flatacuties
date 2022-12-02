3b07873f704b9925ca01b414f2384a57c450ce16
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-03 00:45:06 +0300

    Fetched the json file and applied to code

--- script.js
+++ script.js
@@ -1,98 +1,96 @@
 document.addEventListener("DOMContentLoaded", function () {
-    handleData();
+    fetchData();
 })
-
-function handleData() {
-    // fetch data here
-    const cartoons = [
-        {
-        id:1,
-        name: "pup",
-        imageSrc: "https://media2.giphy.com/media/rgq4Sar77s85W/giphy.gif?cid=ecf05e479b0fhn5x7ezokgh8v34fubi129l8wzpw4vxuy95c&rid=giphy.gif&ct=g ",
-        count: 0
-        },
-        {
-        id:2,
-        name: "scoobie",
-        imageSrc: "https://media1.giphy.com/media/69FmYZBku9m81vhGH3/200.webp?cid=ecf05e4724lsxj5s68tax6uhrxkyma95q3ij6z5tyk8vcqxk&rid=200.webp&ct=g",
-        count: 0
-        },
-        {
-        id:3,
-        name: "pus",
-        imageSrc: "https://media3.giphy.com/media/YkbRS1WViDoZqBiQNq/200w.webp?cid=ecf05e4724lsxj5s68tax6uhrxkyma95q3ij6z5tyk8vcqxk&rid=200w.webp&ct=g",
-        count: 0
-        },
-        {
-        id:4,
-        name: "pus",
-        imageSrc: "https://media3.giphy.com/media/YkbRS1WViDoZqBiQNq/200w.webp?cid=ecf05e4724lsxj5s68tax6uhrxkyma95q3ij6z5tyk8vcqxk&rid=200w.webp&ct=g",
-        count: 0
-        },
-    ]
-    countVotes(cartoons);
-    // forEach(countVotes)
+function fetchData() {
+    fetch('http://localhost:3000/characters').then(resp => resp.json()).then(cartoons =>countVotes(cartoons))
 }
+
 function countVotes(cartoons) {
-   
     const form = document.querySelector('form')
+    // define cartoon title holder
     const cartoonTitle = document.querySelector('.container-content-width h3');
-    
+    // define cartoon image source
     const cartoonSrc = document.querySelector('.container-content-image img');
-   
+    // define voting buttons
+   const addVote = document.getElementById('addVote');
+    const resetVote = document.getElementById('resetVote');
     
-    // SELECT BUTTONS
+    // target individual buttons
     const buttons = document.querySelectorAll('.container-header-buttons button');
     buttons.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      const clicked = event.target.className;
-      cartoons.forEach((cartoon) => {
-          
-          if (cartoon.id == clicked) {
-            
-              cartoonTitle.textContent = cartoon.name;
-              cartoonSrc.src = cartoon.imageSrc;
-          }
+        btn.addEventListener("click", (event) => {
+            const clicked = event.target.className;
+            cartoons.forEach((cartoon) => {
+                if (cartoon.id == clicked) {
+                    cartoonTitle.textContent = cartoon.name;
+                    cartoonSrc.src = cartoon.image;
+                    // voting
+                    let count = cartoon.votes;
+
+                    addVote.addEventListener('click', function (event) {
+                        event.preventDefault();
+                        const countDisplay = document.querySelector('#countDisplay span');
+                        
+                        if (input === '') {
+                            inputPlace.placeholder = "Add your vote in numbers";
+                            countDisplay.textContent = cartoon.votes;
+
+                        } else {
+                            let input = document.querySelector('form div input').value;
+                            let inputValue = parseInt(input, 10);
+                            count += inputValue;
         
-})
-});
-});
+                            countDisplay.textContent = count;
+                            cartoon.votes = count;
+                            // form.reset(); 
+                        };
+        
 
-    
+                    });
+                    resetVote.addEventListener('click', (event) => {
+                            event.preventDefault();
 
-
+                            count = 0;
+                            countDisplay.textContent = count;
+                            cartoon.votes = count;
+    })
+                }
+            });
+        });
+    });
     // VOTING
    
-    const addVote = document.getElementById('addVote');
-   
-    const resetVote = document.getElementById('resetVote');
-   
-   let count = 0;
-       
-    addVote.addEventListener('click', function(event){
-        event.preventDefault();
-        const countDisplay = document.querySelector('#countDisplay span');
-        let inputPlace = document.querySelector('form div input');
-        let input = inputPlace.value;
+    // addVote.addEventListener('click', function(event){
+    //     event.preventDefault();
+    //     const cartoonArray = Array.from(cartoons);
+    //     cartoonArray.reduce(addCounts, 0)
+    //     function addCounts(init, cartoon) {
+    //         let count = cartoon.votes; 
+    //         console.log(count)
+    //         console.log(init)
+    //     }
         
-        if (input === '') {
-            inputPlace.placeholder = "Add your vote in numbers";
-            countDisplay.textContent = count;
+        // cartoons.forEach((cartoon) => {
+        //     let count = cartoon.votes;
+        // console.log(count)
+        //     const countDisplay = document.querySelector('#countDisplay span');
+        //     let inputPlace = document.querySelector('form div input');
+        //     let input = inputPlace.value;
+            
+        //      if (input === '') {
+        //     inputPlace.placeholder = "Add your vote in numbers";
+        //     countDisplay.textContent = count;
 
-        } else {
-           let inputValue = parseInt(input, 10);
-        count += inputValue;
-         countDisplay.textContent = count;
-        // form.reset(); 
-        }
-    })
-    resetVote.addEventListener('click', (event) => {
-        event.preventDefault();
-        count = 0;
-            countDisplay.textContent = count;
-    })
-
-
+        // } else {
+        //    let inputValue = parseInt(input, 10);
+        // count += inputValue;
+        //  countDisplay.textContent = count;
+        // // form.reset(); 
+        // }
+        // })
+        
+    // })
+    
 }
 
 
