b3092359471a9d21c6ca73e7f5760abcc687f8be
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-05 04:56:38 +0300

    Functionality not working well. Started on the js file again

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetchData();
-})
-function fetchData() {
-    fetch('http://localhost:3000/characters').then(resp => resp.json()).then(cartoons =>countVotes(cartoons))
-}
-
-function countVotes(cartoons) {
-    const form = document.querySelector('form')
+const form = document.querySelector('form')
     // define cartoon title holder
     const cartoonTitle = document.querySelector('.container-content-width h3');
     // define cartoon image source
@@ -17,80 +9,10 @@ function countVotes(cartoons) {
     
     // target individual buttons
     const buttons = document.querySelectorAll('.container-header-buttons button');
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", (event) => {
-            const clicked = event.target.className;
-            cartoons.forEach((cartoon) => {
-                if (cartoon.id == clicked) {
-                    cartoonTitle.textContent = cartoon.name;
-                    cartoonSrc.src = cartoon.image;
-                    // voting
-                    let count = cartoon.votes;
-
-                    addVote.addEventListener('click', function (event) {
-                        event.preventDefault();
-                        const countDisplay = document.querySelector('#countDisplay span');
-                        
-                        if (input === '') {
-                            inputPlace.placeholder = "Add your vote in numbers";
-                            countDisplay.textContent = cartoon.votes;
-
-                        } else {
-                            let input = document.querySelector('form div input').value;
-                            let inputValue = parseInt(input, 10);
-                            count += inputValue;
-        
-                            countDisplay.textContent = count;
-                            cartoon.votes = count;
-                            // form.reset(); 
-                        };
-        
-
-                    });
-                    resetVote.addEventListener('click', (event) => {
-                            event.preventDefault();
-
-                            count = 0;
-                            countDisplay.textContent = count;
-                            cartoon.votes = count;
-    })
-                }
-            });
-        });
-    });
-    // VOTING
-   
-    // addVote.addEventListener('click', function(event){
-    //     event.preventDefault();
-    //     const cartoonArray = Array.from(cartoons);
-    //     cartoonArray.reduce(addCounts, 0)
-    //     function addCounts(init, cartoon) {
-    //         let count = cartoon.votes; 
-    //         console.log(count)
-    //         console.log(init)
-    //     }
-        
-        // cartoons.forEach((cartoon) => {
-        //     let count = cartoon.votes;
-        // console.log(count)
-        //     const countDisplay = document.querySelector('#countDisplay span');
-        //     let inputPlace = document.querySelector('form div input');
-        //     let input = inputPlace.value;
-            
-        //      if (input === '') {
-        //     inputPlace.placeholder = "Add your vote in numbers";
-        //     countDisplay.textContent = count;
-
-        // } else {
-        //    let inputValue = parseInt(input, 10);
-        // count += inputValue;
-        //  countDisplay.textContent = count;
-        // // form.reset(); 
-        // }
-        // })
-        
-    // })
-    
+document.addEventListener("DOMContentLoaded", function () {
+    fetchData();
+})
+function fetchData() {
+    fetch('http://localhost:3000/characters').then(resp => resp.json()).then(cartoons =>countVotes(cartoons))
 }
-
 
